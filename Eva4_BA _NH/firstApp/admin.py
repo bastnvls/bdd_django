19e72a6bbbbc6039ab16971006a8b549a49e3083
bastnvls <aviles.bastn@gmail.com>
2024-05-06 14:30:01 -0400

    Delete Eva4_BA _NH directory

--- Eva4_BA _NH/firstApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Producto
-# Register your models here.
-
-@admin.register(Producto)
-class ProductoAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'precio', 'descripcion','ram','procesador','grafica','almacenamiento','unidad_almacenamiento','tipo_almacenamiento','refrigeracion_cpu','unidad_optica','teclado','mouse','sistema_operativo','placa','gabinete','fuente']
--- Eva4_BA _NH/secondApp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from firstApp.models import Producto
-from django.contrib.auth.models import Group, User
-# Create your models here.
-#Descomentar este codigo, luego de realizar la primera migración, ya que 
-#necesita la tabla auth_user para ser valido y crear el grupo Administradores.
-group, created = Group.objects.get_or_create(name='Administradores') 
