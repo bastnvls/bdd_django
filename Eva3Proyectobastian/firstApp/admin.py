37386fdd9798af14268a8ae01f9b16113fc688ea
bastnvls <aviles.bastn@gmail.com>
2024-05-06 14:29:51 -0400

    Delete Eva3Proyectobastian directory

--- Eva3Proyectobastian/firstApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Producto
-# Register your models here.
-
-@admin.register(Producto)
-class ProductoAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'precio', 'descripcion','ram','procesador','grafica','memoria','sistema_operativo','placa','gabinete','fuente']
